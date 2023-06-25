d474b697a618fec238fba845b6136b131b9abb71
cassia012 <cassia012@kakao.com>
2023-06-25 11:37:27 +0900

    추천시스템

---
+++ src/main/java/dev/mvc/recommend/RecommendProc.java
@@ -0,0 +1,29 @@
+package dev.mvc.recommend;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import dev.mvc.recipe.RecipeDAOInter;
+
+@Component("dev.mvc.recommend.RecommendProc")
+public class RecommendProc implements RecommendProcInter {
+    @Autowired
+    private RecommendDAOInter recommendDAO;
+
+    @Override
+    public ArrayList<RecommendVO> recommend(int itemno) {
+        ArrayList<RecommendVO> list = this.recommendDAO.recommend(itemno);
+        return list;
+    }
+
+    @Override
+    public int recommend_read(int memberno) {
+        int recommendVO = this.recommendDAO.recommend_read(memberno);
+        return recommendVO;
+    }
+
+
+
+}
